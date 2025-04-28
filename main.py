23034e957f07017d9b12fffd1c36bf071c91c401
CoolSchnoodle <142824566+CoolSchnoodle@users.noreply.github.com>
2025-04-28 02:27:38 +0000

    Initial ELO model

---
+++ main.py
@@ -0,0 +1,162 @@
+import os
+import itertools
+from typing import Never
+
+DEFAULT_RATING: int = 1000
+FULL_WIN_VSCC_DIFF: int = 25
+
+ratings: dict[str, int] = {}
+
+class Player:
+    def __init__(self, name: str, vscc_percent: int, k: int):
+        self.name = name
+        self.vscc_percent = vscc_percent
+        self.k = k
+
+class Modification:
+    def __init__(self, modifications: dict[str, int], timestamp: int, vscc_results: list[Player] = []):
+        self.modifications: dict[str, int] = modifications
+        self.application_time: int = timestamp
+        self.vscc_results = { p.name: p.vscc_percent for p in vscc_results } if len(vscc_results) > 0 else None
+    
+    def apply(self):
+        for player, score_change in self.modifications.items():
+            ratings[player] += score_change
+
+    def log(self, path: str):
+        with open(path, "w") as f:
+            if self.vscc_results is not None:
+                for player, score_change in self.modifications.items():
+                    f.write(f"{player:16}{ratings[player]:4} -> {ratings[player]+score_change:4} ({score_change:+d}) [vscc% {self.vscc_results[player]}]")
+            else:
+                for player, score_change in self.modifications.items():
+                    f.write(f"{player:16}{ratings[player]:4} -> {ratings[player]+score_change:4} ({score_change:+d})")
+    
+    def log_apply(self, path: str):
+        self.log()
+        self.apply()
+
+def outcome(vscc_1: int | float, vscc_2: int | float) -> float:
+    return ((max(-FULL_WIN_VSCC_DIFF, min(vscc_1 - vscc_2, FULL_WIN_VSCC_DIFF))
+        + FULL_WIN_VSCC_DIFF)
+        / (FULL_WIN_VSCC_DIFF * 2))
+
+class Game:
+    def __init__(self, start_t: float, end_t: float, victor_count: int, results: list[(str, int)], name: str):
+        self.start_t: int = int(start_t*100)
+        self.end_t: int = int(end_t*100)
+        self.victor_count: int = victor_count
+        results_copy: list[(str, int)] = results.copy()
+        results_copy.sort(lambda t: t[1], True)
+        self.results: list[Player] = []
+        self.name = name
+        for i, (player, vscc_percent) in enumerate(results_copy):
+            k = 35 + ((25 / self.victor_count) if i < self.victor_count else 0)
+            self.results.append(Player(player, vscc_percent, k))
+    
+    def score(self, ratings: dict[str, int]) -> Modification:
+        modifications: dict[str, int] = {}
+        for p1, p2 in itertools.combinations(self.results, 2):
+            if p1.name not in ratings:
+                ratings[p1.name] = DEFAULT_RATING
+            if p2.name not in ratings:
+                ratings[p2.name] = DEFAULT_RATING
+            if p1.name not in modifications:
+                modifications[p1.name] = 0
+            if p2.name not in modifications:
+                modifications[p2.name] = 0
+            
+            expected = expected_outcome(ratings[p1.name] - ratings[p2.name])
+            actual = outcome(p1.vscc_percent, p2.vscc_percent)
+            difference = expected - actual
+            modifications[p1.name] += difference * p1.k
+            modifications[p2.name] -= difference * p2.k
+        return Modification(modifications, self.end_t)
+
+def error(message: str) -> Never:
+    print(message)
+    exit(1)
+
+def expected_outcome(diff: int | float) -> float:
+    return 1 / (1 + 2 ** (-diff/1000))
+
+
+games: list[Game] = []
+games_location = input("What folder are your game info files in?\n>>> ")
+game_files = [file_name for file_name in os.listdir(games_location) if os.path.isfile(file_name)]
+
+
+for path in game_files:
+    vsccs: list[(str, int)] = []
+    start_t, end_t, victor_count = None, None, None
+
+    with open(path) as f:
+
+        lines: list[str] = f.readlines()
+        if len(lines) <= 4:
+            error("Error: too few lines in {path}. Each file must at least have 3 lines for the "
+                  "start time, end time, and victor count (0 for a draw), in that order, and then "
+                  "data for at least two countries.")
+        try:
+            start_t = float(lines.pop(0))
+            assert start_t >= 0
+            end_t = float(lines.pop(0))
+            assert end_t >= 0
+            victor_count = int(lines.pop(0))
+            assert victor_count >= 0
+        except Exception:
+            error("Error: header in file {path} was not formatted correctly. Files need a header "
+                  "with start time (month.day), end time (month.day), and victor count (0 for draw), "
+                  "in that order. \"month\" means number of months since January 2024, \"day\" means "
+                  "2-digit day of the month. For example, a start time could be 13.01, meaning January "
+                  "1, 2025.")
+        line_counter: int = 3
+        for line in lines:
+            line_counter += 1
+
+            parts = line.split('\t')
+            if len(parts) != 2:
+                error("Error: file {path} line #{line_count} had the wrong format. "
+                      "Each line after the header should contain the original player discord tag, "
+                      "final player discord tag, and vscc%, in that order, with tabs between them.")
+            
+            original_player, score = parts
+            for o_p, _ in vsccs:
+                if o_p == original_player:
+                    error("Error: file {path} contained two nations played by the same player.")
+            try:
+                score = int(score)
+            except Exception:
+                error("Error: file {path} line {line_count}'s score (third item) was not a number. "
+                      "Each line after the header should contain the original player discord tag,"
+                      "final player discord tag, and vscc%, in that order, with tabs between them.")
+            vsccs.append((original_player, score))
+    if len(vsccs) < 24 or len(vsccs) > 25:
+        print("Warning: file {path} contained information for less than 24 or over 25 players. "
+              "Imperial diplomacy has 24-25 players (depending on the wave), so this seems likely"
+              "to be a mistake.")
+    games.append(Game(start_t, end_t, victor_count, vsccs, "".join(path.split('.')[:-1])))    
+
+modifications_stack: list[(Modification, str)] = []
+the_time: int = -1
+games.sort(lambda g: g.start_t * 10000)
+while True:
+    the_time += 1
+
+    for _ in range(len(modifications_stack)):
+        if modifications_stack[0][0].application_time == the_time:
+            m = modifications_stack.pop(0)
+            m[0].log_apply(m[1] + "_results.txt")
+
+    for _ in range(len(games)):
+        if games[0].start_t == the_time:
+            game = games.pop(0)
+            to_append = game.score(ratings), game.name
+            modifications_stack.append(to_append)
+        else:
+            break # `games` is sorted by `start_t`
+    
+    if len(games) == 0:
+        for modification, output_file in modifications_stack:
+            modification.log_apply(output_file)
+        break
